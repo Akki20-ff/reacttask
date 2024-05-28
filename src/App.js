ed2ace916302fc541e07bc2c024b0ba4faab0608
Akki22-ff <goudankit9720.com>
2024-05-28 16:05:57 +0530

    change on 28-5-24

--- src/App.js
+++ src/App.js
@@ -77,16 +77,58 @@ function App() {
                   <TooltipProvider key={index}>
                     <Tooltip>
                       <TooltipTrigger>
-                        {data.data[index].status === false ?
-                          <div className="p-1 h-12 border-white border-r-4 bg-zinc-400 hover:bg-sky-700"></div>
-                          :
-                          <div className="p-1 h-12 border-white border-r-4 bg-emerald-500 hover:bg-sky-700"></div>
-                        }
+                        {(() => {
+                          if (data.data[index].status === false) {
+                            return <div className="p-1 h-12 border-white border-r-4 bg-zinc-400 hover:bg-sky-700"></div>;
+                          } else if (data.data[index].redalert !== '') {
+                            return <div className="p-1 h-12 border-white border-r-4 bg-red-500 hover:bg-sky-700"></div>;
+                          } else if (data.data[index].yelloalert !== '') {
+                            return <div className="p-1 h-12 border-white border-r-4 bg-yellow-500 hover:bg-sky-700"></div>;
+                          } else {
+                            return <div className="p-1 h-12 border-white border-r-4 bg-emerald-500  hover:bg-sky-700"></div>;
+                          }
+                        })()}
                       </TooltipTrigger>
                       <TooltipContent className="bg-white" >
-                        {data.data[index].status === false ? <p>No data exists for this day.</p> : <p> No downtime recorded on this day. </p>}
-                        <p>{data.data[index].date}</p>
+                        {/* {data.data[index].status === false ? <p>No data exists for this day.</p> : <p> No downtime recorded on this day. </p>}
+                        <p>{data.data[index].date}</p> */}
+                        {(() => {
+                          if (data.data[index].status !== true) {
+                            return <div className="p-4 ">
+                              <p className="mb-4 font-medium">{data.data[index].date} </p>
+                              <p> No downtime recorded on this day. </p>
+                            </div>;
 
+                          } else if (data.data[index].redalert !== '') {
+                            return <div className="p-4 ">
+                              <p className="mb-4 font-medium">{data.data[index].date} </p>
+
+                              <p className="mb-4 font-medium flex text-base">
+                                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 pr-1 text-yellow-500">
+                                  <path fillRule="evenodd" d="M11.484 2.17a.75.75 0 0 1 1.032 0 11.209 11.209 0 0 0 7.877 3.08.75.75 0 0 1 .722.515 12.74 12.74 0 0 1 .635 3.985c0 5.942-4.064 10.933-9.563 12.348a.749.749 0 0 1-.374 0C6.314 20.683 2.25 15.692 2.25 9.75c0-1.39.223-2.73.635-3.985a.75.75 0 0 1 .722-.516l.143.001c2.996 0 5.718-1.17 7.734-3.08ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75ZM12 15a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75v-.008a.75.75 0 0 0-.75-.75H12Z" clipRule="evenodd" />
+                                </svg>
+
+                                {data.data[index].redalert}</p>
+                            </div>;
+
+                          } else if (data.data[index].yelloalert !== '') {
+                            return <div className="p-4 ">
+                              <p className="mb-4 font-medium">{data.data[index].date} </p>
+
+                              <p className="flex text-base mb-4 font-medium"><svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" className="size-6 pr-1 text-yellow-500">
+                                  <path fillRule="evenodd" d="M11.484 2.17a.75.75 0 0 1 1.032 0 11.209 11.209 0 0 0 7.877 3.08.75.75 0 0 1 .722.515 12.74 12.74 0 0 1 .635 3.985c0 5.942-4.064 10.933-9.563 12.348a.749.749 0 0 1-.374 0C6.314 20.683 2.25 15.692 2.25 9.75c0-1.39.223-2.73.635-3.985a.75.75 0 0 1 .722-.516l.143.001c2.996 0 5.718-1.17 7.734-3.08ZM12 8.25a.75.75 0 0 1 .75.75v3.75a.75.75 0 0 1-1.5 0V9a.75.75 0 0 1 .75-.75ZM12 15a.75.75 0 0 0-.75.75v.008c0 .414.336.75.75.75h.008a.75.75 0 0 0 .75-.75v-.008a.75.75 0 0 0-.75-.75H12Z" clipRule="evenodd" />
+                                </svg>
+                                {data.data[index].yelloalert}</p>
+                            </div>;
+
+                          } else {
+                            return <div className="p-4 ">
+                              <p className="mb-4 font-medium">{data.data[index].date} </p>
+
+                              <p>No data exists for this day.</p>
+                            </div>;
+                          }
+                        })()}
                       </TooltipContent>
                     </Tooltip>
                   </TooltipProvider>
